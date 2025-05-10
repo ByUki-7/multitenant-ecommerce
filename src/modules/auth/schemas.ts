d2418f13b3b7e3b3847b497c172540a0191b7445
ByUki-7 <jude.thurman11@gmail.com>
2025-05-10 10:36:21 +0200

    Added authentication

---
+++ src/modules/auth/schemas.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+})
+
+export const registerSchema = z.object({
+        email: z.string().email(),
+        password: z.string().min(3, "Password must be at least 3 characters long"),
+        username: z
+            .string()
+            .min(3, "Username must be at least 3 characters long")
+            .max(63, "Username must be less than 63 characters long")
+            .regex(/^[a-z0-9][a-z0-9-]*[a-z0-9]$/, "Username must contain only lowercase letters, numbers and hyphens. It must start and end with a letter or number")
+            .refine(
+                (val) => !val.includes("--"),
+                "Username cannot contain consecutive hyphens"
+            )
+            .transform((val) => val.toLowerCase()),
+});
+
