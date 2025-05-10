ef21f96f1ea56ceaf2b40eabc7cd32f5f060b6cd
ByUki-7 <jude.thurman11@gmail.com>
2025-05-10 22:31:01 +0200

    Added auth states

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -10,6 +10,8 @@ import { MenuIcon } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -55,6 +57,9 @@ export const Navbar = () => {
     const pathname = usePathname();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
+
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
@@ -81,13 +86,25 @@ export const Navbar = () => {
                 ))}
             </div>
 
-            <div className="hidden lg:flex">
+            {session.data?.user ? (
+                <div className="hidden lg:flex">
+                     <Button
+                        asChild
+                        className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black hover:bg-zantora-blue !text-white hover:!text-black transition-colors text-large"
+                    >
+                        <Link href="/admin">
+                            Dashboard
+                        </Link>
+                    </Button>
+                </div>
+            ) : (
+                <div className="hidden lg:flex">
                     <Button
                         asChild
                         variant="secondary"
                         className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-zantora-blue transition-colors text-large"
                     >
-                        <Link href="/sign-in">
+                        <Link prefetch href="/sign-in">
                             Log in
                         </Link>
                     </Button>
@@ -95,11 +112,12 @@ export const Navbar = () => {
                         asChild
                         className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black hover:bg-zantora-blue !text-white hover:!text-black transition-colors text-large"
                     >
-                        <Link href="/sign-up">
+                        <Link prefetch href="/sign-up">
                             Start selling
                         </Link>
                     </Button>
-            </div>
+                 </div>
+            )}
 
             <div className="flex lg:hidden item-center justify-center">
                 <Button
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -6,7 +6,7 @@ import { toast } from "sonner"
 import { Poppins } from "next/font/google"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -33,11 +33,14 @@ export const SignInView = () => {
     const router = useRouter();
 
     const trpc = useTRPC();
+    const queryClient = useQueryClient();
     const login = useMutation(trpc.auth.login.mutationOptions({
         onError: (error) => {
             toast.error(error.message)
         },
-        onSuccess: () => {
+        onSuccess: async () => {
+            await queryClient.invalidateQueries(trpc.auth.session.queryFilter());
+            await queryClient.refetchQueries(trpc.auth.session.queryFilter());
             router.push("/")
         },
     }))
@@ -81,7 +84,7 @@ export const SignInView = () => {
                             </Button>
                         </div>
                         <h1 className="text-4xl font-medium">
-                            Welcome back to zantora
+                            Welcome back to 
                         </h1>
                         <FormField 
                             name="email"
