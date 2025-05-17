7a4dee11f50cc178b748949da65c7af7f5e06512
ByUki-7 <jude.thurman11@gmail.com>
2025-05-17 13:24:06 +0200

    Added the products db module

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,10 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+ 
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+
+import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
+import { Suspense } from "react";
+
 interface Props {
     params: Promise<{
         category: string;
@@ -5,12 +12,20 @@ interface Props {
 }
 
 const Page = async ({ params }: Props) => {
+
     const { category } = await params;
 
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
+        category,
+    }));
+
     return ( 
-        <div>
-            Category: {category}
-        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<ProductListSkeleton />}>
+                <ProductList category={category} />
+            </Suspense>
+        </HydrationBoundary>
     );
 };
  
