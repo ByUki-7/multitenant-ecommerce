e35db2039b49074749d842f01c0d4ded6504ffbb
ByUki-7 <jude.thurman11@gmail.com>
2025-08-14 15:15:35 +0200

    Added the reviews

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -7,6 +7,7 @@ import { productsRouter } from '@/modules/products/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
 import { checkoutRouter } from '@/modules/checkout/server/procedures';
 import { libraryRouter } from '@/modules/library/server/procedures';
+import { reviewsRouter } from '@/modules/reviews/server/procedures';
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
   products: productsRouter,
   categories: categoriesRouter,
   library: libraryRouter,
+  reviews: reviewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
